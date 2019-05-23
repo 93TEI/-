503276629bd2490aea74c40101091b01a4d03fbe
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-05-24 00:01:06 +0900

    index를 붙여 바로 찾고 넣을 수 있도록 구현

--- mappingTest.sol
+++ mappingTest.sol
@@ -1,16 +1,33 @@
 pragma solidity ^0.4.11;
 
 contract mappingTest{
-    mapping(address => bytes32) public map;
+    struct entry{
+        uint index;
+        bytes32 value;
+    }
+    
+    mapping(address => entry) internal map;
     address[] internal keylist; // for check out how many keys in map
     
     function add(address _key, bytes32 _value) public {
-        map[_key] = _value;
-        keylist.push(_key);
+        entry en = map[_key];
+        en.value = _value;
+        
+        if(en.index > 0){ //exist
+            return;
+        } else {
+            keylist.push(_key);
+            uint keylistIndex = keylist.length - 1 ;
+            en.index = keylistIndex + 1 ;
+        }
     }
     
     function del(address _key) public {
         delete map[_key];
+    }
+    
+    function contain() public returns(bool){ // exist
+        return map[_key].index > 0 ;
     }
     
     function checkoutlen() view public returns(uint){
