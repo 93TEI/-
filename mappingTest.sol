7359a6e12aa02b86725898d684f6dc4efa3b4e1a
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-05-23 22:52:01 +0900

    mapping에서 key가 몇개있는지 제공하지 않기 때문에 그 기능을 만들어줌

--- mappingTest.sol
+++ mappingTest.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.11;
 
 contract mappingTest{
     mapping(address => bytes32) public map;
+    address[] internal keylist; // for check out how many keys in map
     
     function add(address _key, bytes32 _value) public {
         map[_key] = _value;
@@ -9,6 +10,10 @@ contract mappingTest{
     
     function del(address _key) public {
         delete map[_key];
+    }
+    
+    function checkoutlen() view public returns(uint){
+        return uint(keylist.length); // ? is this Çüº¯È¯?
     }
     
     function show(address _key) constant returns(bytes32){
