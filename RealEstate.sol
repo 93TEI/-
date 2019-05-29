971a2b7c280e9369c6a85737dd46094eaa8faded
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-05-30 00:52:50 +0900

    complete

--- RealEstate.sol
+++ RealEstate.sol
@@ -15,17 +15,14 @@ contract RealEstate{
         uint propertyPrice;
         bool propertyState;
     }
-    event LogBuyRealEstate(
-    );
-    
-    event LogSellRealEstate(
-    );
+    event LogBuyRealEstate(address buyderAddress, uint propertyId);
+    event LogSellRealEstate(string propertyType,string propertyName,string propertyAddress,uint constructionYear,uint propertyPrice,bool propertyState);
     
     mapping (uint => Buyer) public buyerInfo;
     mapping (uint => Property) public propertyInfo;
     
     constructor() public {
-        owner = msg.sender; //temp
+        owner = msg.sender;
     }
     
     modifier onlyOwner {
@@ -35,22 +32,34 @@ contract RealEstate{
     
     function RegisterRealEstate(string memory _propertyType, string memory _propertyName, string
     memory _propertyAddress, uint _constructionYear, uint _propertyPrice) public onlyOwner {
+        emit LogSellRealEstate(_propertyType, _propertyName, _propertyAddress, _constructionYear, _propertyPrice, true);
     }
     
     function buyRealEstate(uint _propertyId, string memory _buyerName, uint _buyerAge) public
     payable {
         require(_propertyId >= 0); //Check the property identity is equal or greater than zero
-        require(msg.value == propertyPrice); //  Check whether a message sender has sent the same amount  of Ethereum as the real estate price
-        require(propertyState); //Check whether the real estate property chosen by a message sender is sold out or not
-        Buyer b = {};
-        buyerInfo[] = b;
+        require(msg.value == Property[_propertyId].propertyPrice); //  Check whether a message sender has sent the same amount  of Ethereum as the real estate price
+        require(Property[_propertyId].propertyState); //Check whether the real estate property chosen by a message sender is sold out or not
+        owner.transfer(msg.value); // Transfer Ether to the owner
+        
+        Buyer b = buyerInfo[_propertyId]; //  All information of buyers are stored in blockchain
+        b.buyerAddress = msg.sender;
+        b.buyerName = _buyerName;
+        b.buyerAge = _buyerAge;
+        
+        //event
+        emit LogBuyRealEstate(msg.sender,_propertyID);
     }
     
     function getRealEstate(uint _propertyID) public view returns (string memory, string memory,
     string memory, uint, uint)
     {
+        Property p = propertyInfo[_propertyID];
+        return (p.propertyType, p.propertyName, p.propertyAddress, p.constructionYear, p.propertyPrice, p.propertyState);
     }
     function getBuyer(uint _propertyID) public view returns (address, string memory, uint)
     {
+        Buyer by = buyerInfo[_propertyID];
+        return (by.buyerAddress, by.buyerName, by.buyerAge);
     }
 }
