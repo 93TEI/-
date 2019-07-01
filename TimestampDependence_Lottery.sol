4e58c6d0cc8fb231998a4f52bc2be0da6e831ab9
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-07-01 23:28:05 +0900

    단순한 추첨 예제

--- TimestampDependence_Lottery.sol
+++ TimestampDependence_Lottery.sol
@@ -18,9 +18,29 @@ contract TimestampDependence_Lottery{
         numApplicants = 0;
     }
 
-    //추첨 응모 처리 함수
     function enter() public {
         //응모자가 3명 미만인지 확인
-        
+        require(numApplicants < 3);
+
+        //이미 응모한 사람이 아닌지 확인
+        for(uint i=0; i<numApplicants; i++){
+            require(applicants[i] != msg.sender);
+        }
+
+        //응모 접수 처리
+        applicants[numApplicants++] = msg.sender;
+    }
+
+    // 추첨
+    function hold() public onlyOwner {
+        //응모자가 3명 이상인지 확인 
+        require(numApplicants == 3);
+
+        //타임스탬프 값 설정
+        timestamp = block.timestamp;
+
+        //추첨
+        winnerInd = timestamp % 3 ;
+        winnerAddress = applicants[winnerInd];
     }
 }
